105377d47334e0ab9d966f7a8e62797964e32b40
John Linahan <linahan.john@gmail.com>
2017-10-26 14:06:16 -0400

    Player turns cycle

--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -4,8 +4,11 @@ import javax.swing.*;
 
 public class Token extends JButton {
 
-  public Token(int playernum) {
-    this.setText("" + playernum);
+  private final int player_index;
+
+  public Token(int player_index) {
+    this.player_index = player_index;
+    this.setText("" + (player_index + 1));
     this.setPreferredSize(new Dimension(20, 20));
     this.setBorder(null);
     this.addActionListener(new ButtonListener());
@@ -17,17 +20,11 @@ public class Token extends JButton {
   private class ButtonListener implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
-      Game game = Game.getInstance();
-      MessagePanel messagePanel = game.getMessagePanel();
-      messagePanel.setMessage("Next Player's Turn");
-      // TODO
-      // somehow check that the square that was clicked matches the card the player got
-      // move the current player's token
-      // if the current player is at Grandma's house
-      // display message - this player wins
-      // else
-      // switch to next player
-      // display message - next player's turn
+      if(Game.current_turn == player_index)
+      {
+        Game game = Game.getInstance();
+        game.nextTurn();
+      }
     }
   }
 }
